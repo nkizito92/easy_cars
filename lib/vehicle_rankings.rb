ec6b5a5f90da93d94922d94746e134432f69f864
Kizito Njoku <nkizito57@gmail.com>
2019-12-02 21:12:57 +0000

    first commit

---
+++ lib/vehicle_rankings.rb
@@ -0,0 +1,6 @@
+require "vehicle_rankings/version"
+
+module VehicleRankings
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vehicle_rankings/version.rb
@@ -0,0 +1,3 @@
+module VehicleRankings
+  VERSION = "0.1.0"
+end
